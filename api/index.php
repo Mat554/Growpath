3a8cea248f73cd8f41bda30573d4ee0d519a80ff
Mat554 <matiusrizky25@gmail.com>
2026-05-05 10:23:20 +0700

    Feat: Moved the Storage folder to tmp

--- api/index.php
+++ api/index.php
@@ -1,3 +1,16 @@
 <?php
-// Meneruskan request dari Vercel ke sistem public Laravel
-require __DIR__ . '/../public/index.php';
+
+require __DIR__ . '/../vendor/autoload.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+// --- TAMBAHKAN BARIS INI UNTUK VERCEL ---
+// Mengarahkan folder storage ke /tmp yang diizinkan oleh Vercel
+$app->useStoragePath($_ENV['APP_STORAGE'] ?? '/tmp/storage');
+// ----------------------------------------
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+$response->send();
+$kernel->terminate($request, $response);
