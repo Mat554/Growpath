d579c5522d5f6c8910a1cacf18b527d0abc07045
Mat554 <matiusrizky25@gmail.com>
2026-05-05 15:03:01 +0700

    Feat: fixed index php

--- api/index.php
+++ api/index.php
@@ -1,16 +1,3 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-
-// --- TAMBAHKAN BARIS INI UNTUK VERCEL ---
-// Mengarahkan folder storage ke /tmp yang diizinkan oleh Vercel
-$app->useStoragePath($_ENV['APP_STORAGE'] ?? '/tmp/storage');
-// ----------------------------------------
-
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-$response->send();
-$kernel->terminate($request, $response);
